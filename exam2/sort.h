989181ff02b09d72547753183266a98c99718198
Reputet <outofspace230@gmail.com>
2021-06-26 03:39:02 +0300

    Add a program that sorts a 2D array by word length

---
+++ exam2/sort.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+string get_input(string);
+int get_int (string);
+void input_array(string *, int, int);
+void swap_elements(string *, string *);
+void print_array(string *, int, int);
+void sort(string *, int, int);
