ba89ef39d4af56d96791051c65bf6897e24c1f6c
Reputet <outofspace230@gmail.com>
2022-02-16 23:22:44 +0300

    Finish the program

--- lab4-1/main.cc
+++ lab4-1/main.cc
@@ -1,61 +1,105 @@
-#define _USE_MATH_DEFINES
+#include "./main.h"
 
-#include <SFML/Graphics.hpp>
-#include <cmath>
-#include <iostream>
-#include <string>
+const int MAX_WIDTH = 1200;
+const int MAX_HEIGHT = 900;
 
-int main() {
-    float sideA;
-    float sideB;
-    float angle;
+struct Vertex {
+    float x;
+    float y;
+};
 
-    std::cout << "Input edge A: ";
-    std::cin >> sideA;
-    std::cout << "Input edge B: ";
-    std::cin >> sideB;
-    std::cout << "Input angle: ";
-    std::cin >> angle;
-    struct Point {
-        float x;
-        float y;
-    };
+float get_num (string message, int min, int max) {
+    string line;
+    float number;
+    cout << message;
+    while (true) {
+        getline(cin, line);
+        stringstream ss(line);
+        if (ss >> number) {
+            if (ss.eof()) {
+                if (number <= min || number >= max) {
+                    cout << "ERROR: a number must be from " << min << " to " << max << ": ";
+                    continue;
+                }
+                // Success
+                break;
+            }
+        }
+        cout << "ERROR: a number (float) must be entered: ";
+    }
+    return number;
+}
 
-    Point a;
-    Point b;
-    Point c;
-    Point d;
+sf::ConvexShape draw_parallelogram(float x, float y, float edgeA, float edgeB, float angle) {
+    Vertex a;
+    Vertex b;
+    Vertex c;
+    Vertex d;
 
     a.x = 0;
     a.y = 0;
-    b.x = 0;
-    b.y = sideA;
-    c.x = cos((90 - angle) * M_PI / 180) * sideB;
-    c.y = b.y - sin((90 - angle) * M_PI / 180) * sideB;
-    d.x = c.x;
-    d.y = c.y - sideA;
 
-    sf::ContextSettings settings;
-    settings.antialiasingLevel = 16;
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!", sf::Style::Default, settings);
+    b.x = edgeA;
+    b.y = a.y;
+
+    c.x = edgeA + cos(angle * M_PI / 180) * edgeB;
+    c.y = sin(angle * M_PI / 180) * edgeB;
+
+    d.x = c.x - edgeA;
+    d.y = c.y;
 
     sf::ConvexShape parallelogram;
 
     parallelogram.setPointCount(4);
-
     parallelogram.setPoint(0, sf::Vector2f(a.x, a.y));
     parallelogram.setPoint(1, sf::Vector2f(b.x, b.y));
     parallelogram.setPoint(2, sf::Vector2f(c.x, c.y));
     parallelogram.setPoint(3, sf::Vector2f(d.x, d.y));
-    parallelogram.setPosition(300, 300);
-    // parallelogram.rotate(180.f);
-    parallelogram.setScale(30, 30);
-    // parallelogram.setFillColor(sf::Color::Green);
-    // parallelogram.setFillColor(sf::Color::Black);
+
+    // Move close to the center
+    parallelogram.setPosition(x, y);
+    //parallelogram.setScale(30, 30);
+    // parallelogram.rotate(90);
     parallelogram.setFillColor(sf::Color(220, 220, 220));
-    parallelogram.setOutlineThickness(-1.f / 8);
-    // parallelogram.setOutlineColor(sf::Color(250, 150, 100));
+    parallelogram.setOutlineThickness(-4.f);
     parallelogram.setOutlineColor(sf::Color::Black);
+
+    return parallelogram;
+}
+
+int main() {
+
+    float edgeA;
+    float edgeB;
+    float angle;
+    float x;
+    float y;
+
+    stringstream ss;
+
+    ss << "Input edge A length (from 0 to " << MAX_WIDTH/2 << "): ";
+    edgeA = get_num(ss.str(), 0, MAX_WIDTH/2);
+    ss.str("");
+
+    ss << "Input edge B length (from 0 to " << MAX_HEIGHT/2 << "): ";
+    edgeB = get_num(ss.str(), 0, MAX_HEIGHT/2);
+    ss.str("");
+
+    angle = get_num("Input angle (from 0 to 180): ", 0, 180);
+
+    ss << "Input X position of the shape (from " << edgeB << " to " << MAX_WIDTH - edgeA - edgeB << "): ";
+    x = get_num(ss.str(), edgeB, MAX_WIDTH - edgeA - edgeB);
+    ss.str("");
+
+    ss << "Input Y position of the shape (from 0 to " << MAX_HEIGHT - edgeB << "): ";
+    y = get_num(ss.str(), 0, MAX_HEIGHT - edgeB);
+    ss.str("");
+
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 16;
+    sf::RenderWindow window(sf::VideoMode(MAX_WIDTH, MAX_HEIGHT), "Lab1", sf::Style::Default, settings);
+
+    sf::ConvexShape parallelogram = draw_parallelogram(x, y, edgeA, edgeB, angle);
 
     while (window.isOpen())
     {
---
+++ lab4-1/main.h
@@ -0,0 +1,11 @@
+#define _USE_MATH_DEFINES
+
+#include <cmath>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+float get_int (string);
